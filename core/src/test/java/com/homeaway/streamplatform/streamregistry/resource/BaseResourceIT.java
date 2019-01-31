ebcda5a1e335fe74e5d84ec5308244cad5958cc5
Vinayak Ponangi <pponangi@homeaway.com>
2019-01-31 12:54:37 -0600

    Removing /clusters code. Would rather do this fresh instead of transforming current clusters topic

--- core/src/test/java/com/homeaway/streamplatform/streamregistry/resource/BaseResourceIT.java
+++ core/src/test/java/com/homeaway/streamplatform/streamregistry/resource/BaseResourceIT.java
@@ -118,6 +118,8 @@ public class BaseResourceIT {
 
     protected static GlobalKStreams sourceProcessor;
 
+    protected static ClusterResource clusterResource;
+
     protected static StreamResource streamResource;
 
     protected static ConsumerResource consumerResource;
@@ -262,6 +264,9 @@ public class BaseResourceIT {
         StreamClientDao<Consumer> consumerDao = new ConsumerDaoImpl(streamProducer, streamProcessor, env, regionDao,
             infraManager, kafkaManager);
         SourceDao sourceDao = new SourceDaoImpl(sourceProducer, sourceProcessor);
+        ClusterDao clusterDao = new ClusterDaoImpl(env, infraManager);
+
+        clusterResource = new ClusterResource(clusterDao);
         streamResource = new StreamResource(streamDao, producerDao, consumerDao, sourceDao);
         producerResource = new ProducerResource(streamDao, producerDao);
         consumerResource = new ConsumerResource(streamDao, consumerDao);
